7613184aed47a4c2ce4a49dbeb155dc3f4d5f40d
maximebf <maxime.bouroumeau@gmail.com>
2014-10-23 00:39:17 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup, find_packages
 
 
-def desc():
-    with open("README.md") as f:
-        return f.read()
-
-def reqs():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
 setup(
     name='frasco-users',
     version='0.1',
@@ -17,14 +9,21 @@ setup(
     author='Maxime Bouroumeau-Fuseau',
     author_email='maxime.bouroumeau@gmail.com',
     description="Users management for Frasco",
-    long_description=desc(),
     packages=find_packages(),
-    include_package_data=True,
+    package_data={
+        'frasco_users': [
+            'templates/users/*.html',
+            'emails/users/*.txt',
+            'admin/templates/admin/users/*.html']
+    },
     zip_safe=False,
     platforms='any',
-    install_requires=reqs() + [
-        'frasco',
-        'frasco-forms',
-        'frasco-models'
+    install_requires=[
+        # 'frasco',
+        # 'frasco-forms',
+        # 'frasco-models',
+        'Flask-Login>=0.2.11',
+        'Flask-Bcrypt>=0.6.0',
+        'Flask-OAuth>=0.12'
     ]
 )
